b493b76bd8bbcbc92f9a870765800bf53722805a
leof18 <lfriedrich.ieu2019@student.ie.edu>
2020-11-10 00:06:13 +0100

    exit opportunity

--- main.py
+++ main.py
@@ -12,27 +12,29 @@ blank and press enter to continue. If you would like to see a\n\
 list of available ingredients write "show available".\n')
 
     ingredient_list = ingredient_input()
-
-    bst = binary_search_tree(ingredient_list)
-
-    print('\nThe following recipes are recommended based on your preference:')
-
-    recommendations = []
-    io_traverse(bst.root, recommendations)
-    display_recommendations(recommendations)
-
-    print()
-
-    selected_recipe = user_choice(recommendations)
-
-    shopping_list = missing(selected_recipe, recommendations, ingredient_list)
-
-    print('\nHere is a list of the ingredients you are still missing:\n')
-
-    for i in range(len(shopping_list)):
-        print(' -', shopping_list[i].capitalize())
     
-    print('\n--------------- Thank you for using Shoptimize! ---------------\n')
+    if ingredient_list:
+
+        bst = binary_search_tree(ingredient_list)
+
+        print('\nThe following recipes are recommended based on your preference:')
+
+        recommendations = []
+        io_traverse(bst.root, recommendations)
+        display_recommendations(recommendations)
+
+        print()
+
+        selected_recipe = user_choice(recommendations)
+
+        shopping_list = missing(selected_recipe, recommendations, ingredient_list)
+
+        print('\nHere is a list of the ingredients you are still missing:\n')
+
+        for i in range(len(shopping_list)):
+            print(' -', shopping_list[i].capitalize())
+    
+    print('\n---------------- Thank you for using Shoptimize! ----------------\n')
 
 
 if __name__ == '__main__':
